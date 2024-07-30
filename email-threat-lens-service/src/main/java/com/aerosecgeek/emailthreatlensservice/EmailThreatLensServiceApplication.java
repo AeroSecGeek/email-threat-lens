221b320352198726e0291c265fb840a9e89461da
AeroSecGeek <AeroSecGeek@proton.me>
2024-07-30 21:26:34 +0200

    feat #1: basic setup of spring boot project

---
+++ email-threat-lens-service/src/main/java/com/aerosecgeek/emailthreatlensservice/EmailThreatLensServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aerosecgeek.emailthreatlensservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailThreatLensServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailThreatLensServiceApplication.class, args);
+    }
+
+}
